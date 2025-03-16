12b604c429e13f5378be8f34863131e740d6912d
Loknath Dhar <loknath-dhar@pm.me>
2025-03-16 13:46:24 +0600

    introduced goroutine

--- main.go
+++ main.go
@@ -14,35 +14,57 @@ func main() {
 		log.Println("can't open the file")
 	}
 
-	defer file.Close()
+	ch := getLinesChannel(file)
 
-	buffer := make([]byte, 8)
-	currentLine := ""
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-	for {
-		// read the file
-		bytesRead, err := file.Read(buffer)
-		if err != nil && err != io.EOF {
-			log.Println("can't read the file")
-		}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	// create a channel
+	ch := make(chan string)
 
-		if bytesRead == 0 {
-			break
-		}
+	go func() {
+		// creating buffer to read 8 bytes at a time
+		buffer := make([]byte, 8)
+		currentLine := ""
 
-		// add the new bytes to the current line
-		currentLine += string(buffer[:bytesRead])
+		// closing channel when goroutine ends
+		defer close(ch)
+		defer f.Close()
+		for {
+			// read the file
+			bytesRead, err := f.Read(buffer)
+			if err != nil && err != io.EOF {
+				log.Printf("can't read the file: %v", err)
+				return
+			}
 
-		if strings.Contains(currentLine, "\n") {
-			parts := strings.SplitN(currentLine, "\n", 2)
-			fmt.Printf("read: %s\n", parts[0])
-			if len(parts) > 1 {
-				currentLine = parts[1]
-			} else {
-				currentLine = ""
+			if err == io.EOF {
+				break
+			}
+
+			// add the new bytes to the current line
+			currentLine += string(buffer[:bytesRead])
+
+			if strings.Contains(currentLine, "\n") {
+				parts := strings.SplitN(currentLine, "\n", 2)
+
+				ch <- parts[0]
+
+				if len(parts) > 1 {
+					currentLine = parts[1]
+				} else {
+					currentLine = ""
+				}
 			}
 		}
-	}
 
-	fmt.Printf("read: %s\n", currentLine)
+		if currentLine != "" {
+			ch <- currentLine
+		}
+	}()
+
+	return ch
 }
